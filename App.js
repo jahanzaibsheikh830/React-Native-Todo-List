8a7a3ac9d33b21247213b81f8547d2933bd9a61b
asma496 <asmaashraf496@gmail.com>
2021-03-08 17:14:00 +0500

    todo

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, FlatList } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
   },
+  card: {
+    backgroundColor: "#fff",
+    padding: 10,
+    width: "80%",
+    marginTop: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
+  }
 });
+
+export default function App() {
+  const [text, setText] = useState('');
+  const [textArr, setTextArr] = useState([])
+  console.log(text)
+  function addTodo() {
+    setTextArr(prev => [...prev, text])
+  }
+  console.log(textArr)
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={{ marginBottom: 10, fontSize: 20, fontWeight: "bold" }}>Todo List</Text>
+        <TextInput
+          style={{
+            height: 40, paddingLeft: 10,
+            width: '80%', borderColor: 'gray',
+            borderWidth: 1, marginBottom: 10
+          }}
+          onChangeText={(e) => setText(e)}
+          placeholder="Type your notes"
+        />
+        <Button
+          onPress={addTodo}
+          title="Learn More"
+        />
+        {/* <FlatList
+        data={textArr}
+        renderItem={(data) => {
+          return (
+            <View style={styles.card}>
+              <Text>{data.item}</Text>
+            </View>
+          )
+        }}
+      /> */}
+        {
+          textArr.map(data => {
+            return (
+              <View style={styles.card}>
+                <Text>{data}</Text>
+              </View>
+            )
+          })
+        }
+      </View>
+    </ScrollView>
+  );
+}
+
